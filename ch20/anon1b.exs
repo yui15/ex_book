1c74a82b8b6a0a97121cee32134c1c66a47328c1
yui15 <fumufumu1510@gmail.com>
2023-04-25 22:24:45 +0900

    20-2-2

---
+++ ch20/anon1b.exs
@@ -0,0 +1,10 @@
+my_func =
+  fn arg ->
+    case arg do
+      "!" <> name -> %{name: name, type: "admin"}
+      name -> %{name: name, type: "user"}
+    end
+  end
+
+IO.inspect(my_func.("!Alice"))
+IO.inspect(my_func.("Bob"))
---
+++ ch20/anon3.exs
@@ -0,0 +1,18 @@
+players = [
+  %{name: "alice", score: 50},
+  %{name: "bob", score: 80},
+  %{name: "carol", score: 0},
+  %{name: "david", score: 100},
+  %{name: "eve"}
+]
+
+#x =
+#  players
+#  |> Enum.filter(fn
+#    %{score: score} when score >= 50 -> true
+#    _ -> false
+#  end)
+#  |> Enum.map(fn p -> p.name end)
+x = for %{score: score} = p <- players, score >= 50, do: p.name
+
+IO.inspect(x)
